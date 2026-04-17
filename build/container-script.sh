5d18f18e3f3b9eb95ccaee36ed65e377e2998726
TherLaf <therlaf0409@gmail.com>
2026-04-18 00:32:35 +0800

    Added a dedicated steamcmd config text file.

--- build/container-script.sh
+++ build/container-script.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 # Add container setup bash code here...
+
+# SteamCMD configuration file.
+cat > /home/steam/steamcmd-config.txt <<'EOL'
+force_install_dir /home/steam/server-files
+login anonymous
+app_update ###### validate
+quit
+EOL
--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 SERVER_EXECUTABLE="/home/steam/server-files/SERVER_FILE_EXECUTABLE_HERE"
-APP=343050  # DST as Sample
 
 # Download server files if needed.
 if [ ! -f $SERVER_EXECUTABLE ]; then
   echo "Downloading server files..."
-
-  bash /home/steam/steamcmd/steamcmd.sh \
-  +force_install_dir /home/steam/server-files \
-  +login anonymous \
-  +app_update $APP validate \
-  +quit
+  bash /home/steam/steamcmd/steamcmd.sh +runscript /home/steam/steamcmd-config.txt
 fi
 
 # Start the server.
